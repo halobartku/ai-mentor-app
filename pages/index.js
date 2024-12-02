119f550e5d760cc6ea7e0af0c8136d0c1bcf0c93
halobartku <bartosz@easykotlet.com>
2024-12-02 02:46:47 +0100

    Clean up project and fix version conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">AI Mentor</h1>
+    <main>
+      <h1>AI Mentor</h1>
     </main>
   );
 }
