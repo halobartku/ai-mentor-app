ca20a0a2aadcc63a0ddd7408fbd4ac5e7c9d090f
halobartku <bartosz@easykotlet.com>
2024-12-03 23:15:50 +0100

    Delete frontend/app directory

--- frontend/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-import { auth } from '@clerk/nextjs';
-
-export default async function DashboardPage() {
-  const { userId } = auth();
-  if (!userId) redirect('/sign-in');
-
-  return redirect('/dashboard/chat');
-}
