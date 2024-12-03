1b9496a5a8d76558dacb93f8f5470052a50307e9
halobartku <bartosz@easykotlet.com>
2024-12-03 22:57:22 +0100

    Fix import paths and move utils to src directory

--- frontend/src/components/common/Sidebar.tsx
+++ frontend/src/components/common/Sidebar.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { cn } from '@/lib/utils';
+import { cn } from '@/utils';
 
 interface SidebarLink {
   href: string;
